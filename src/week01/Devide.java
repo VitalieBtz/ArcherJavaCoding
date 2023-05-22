eac5c9e91f8311d406de05d2118397d1dddfcce2
anis_k <aanniissk@gmail.com>
2023-05-22 15:59:56 -0400

    tasks added

---
+++ src/week01/Devide.java
@@ -0,0 +1,7 @@
+package week01;
+
+public class Devide {
+    public static int devide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/week01/EvenOrODD.java
@@ -0,0 +1,6 @@
+package week01;
+
+public class EvenOrODD {
+    public  static String oDDorEven(int a){
+        return a%2==0?"is Even ":"is Odd";    }
+}
