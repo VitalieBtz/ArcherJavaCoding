01d9a051d748c1ef9d4faf2a77761e1261e5379b
sara54c <sara5453c@gmail.com>
2023-05-23 10:08:19 -0400

    added some details in OddOrEven Class

--- src/week01/OddOrEven.java
+++ src/week01/OddOrEven.java
@@ -27,5 +27,12 @@ public class OddOrEven {
 
 
 }
+/*
+1️⃣  Numbers -- odd & even
+Write a method that can identify  a given number is even or odd.
+EX:
+       identify(5) --> "Odd"
+       identify(6) --> "Even"
+ */
 
 
