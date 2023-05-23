2cde33d7d95c9be6d120e7dfb40d4c06f0330219
sara54c <sara5453c@gmail.com>
2023-05-23 10:03:45 -0400

    updated OddOrEven class

--- src/week01/OddOrEven.java
+++ src/week01/OddOrEven.java
@@ -1,6 +1,31 @@
 package week01;
 
 public class OddOrEven {
+    public static void main(String[] args) {
+
+        System.out.println(isOdd(5));
+        System.out.println(isEven(6));
+
+
+
+
+    }
+    public static boolean isOdd(int number){
+        if (number %2 != 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean isEven(int number){
+        return !isOdd(number);
+    }
+
+
+
 
 
 }
+
+
