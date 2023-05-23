758638bbde0e9356b3f7bc1733221fe242655f1d
sara54c <sara5453c@gmail.com>
2023-05-23 13:40:26 -0400

    created divide method

--- src/week01/DivideWithoutDivisionOperator.java
+++ src/week01/DivideWithoutDivisionOperator.java
@@ -2,6 +2,40 @@ package week01;
 
 public class DivideWithoutDivisionOperator {
 
+    public static void main(String[] args) {
+
+
+        int dividend = 16;
+        int divisor = 8;
+        int result;
+        int remainder;
+        int count;
+
+
+
+
+    }
+    public static void divide(int dividend, int divisor){
+        int result = 0;
+        if (dividend == 0){
+            System.err.println("you cannot divide by 0");
+            System.exit(1);
+        }else if (dividend == 0){
+            System.out.println(0);
+        }else if (dividend == divisor){
+            System.out.println(1);
+        }else if (dividend>0 && divisor>0 && dividend < divisor){
+            System.out.println(0);
+        }else if (dividend >0 && divisor<0){ // either one of them is positive
+
+        }else{
+
+        }
+    }
+
+
+
+
 
 
 
