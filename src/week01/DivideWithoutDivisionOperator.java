c653dae39f13c7d9a178dde3943f876650e5b8a9
sara54c <sara5453c@gmail.com>
2023-05-23 12:35:47 -0400

    give a condition on this class

--- src/week01/DivideWithoutDivisionOperator.java
+++ src/week01/DivideWithoutDivisionOperator.java
@@ -1,4 +1,13 @@
 package week01;
 
 public class DivideWithoutDivisionOperator {
+
+
+
+
 }
+/*
+2️⃣  Numbers -- Divide without / operator
+Write a method that can divide two numbers without using division operator.
+
+ */
