db94ded200c628fd3f39819a6c97057f4b61eedd
anis_k <aanniissk@gmail.com>
2023-05-24 11:47:10 -0400

    adde

--- src/week01/EvenOrODD.java
+++ src/week01/EvenOrODD.java
@@ -1,6 +1,19 @@
 package week01;
 
 public class EvenOrODD {
-    public  static String oDDorEven(int a){
-        return a%2==0?"is Even ":"is Odd";    }
+    public static String oDDorEven(int a) {
+        return a % 2 == 0 ? "is Even " : "is Odd";
+    }
+
+    //---------------------------------------------------------
+    public static void oddOrEven(int n) {
+
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+    }
+
+
 }
--- src/week01/SampleClass.java
+++
@@ -1,6 +0,0 @@
-package week01;
-
-public class SampleClass {
-
-    // this class was created for the package to have something in it
-}
