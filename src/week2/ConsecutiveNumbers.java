8d126015860d45b5c3df1555a363e103d85543f0
sara54c <sara5453c@gmail.com>
2023-05-30 22:39:02 -0400

    updated consecutiveNumbers class

--- src/week2/ConsecutiveNumbers.java
+++ src/week2/ConsecutiveNumbers.java
@@ -1,6 +1,32 @@
 package week2;
 
 public class ConsecutiveNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            String output = "";
+
+            if (i % 2 == 0) {
+                output += "Codility";
+            }
+            if (i % 3 == 0) {
+                output += "Test";
+            }
+            if (i % 5 == 0) {
+                output += "Coders";
+            }
+
+            if (output.isEmpty()) {
+                System.out.println(i);
+            } else {
+                System.out.println(output);
+            }
+
+
+        }
+    }
+
 
 }
 
