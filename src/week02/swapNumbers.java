e24ebfffa1be9ce9cea14142dafb0ad12a63daa9
VitalieBtz <123746791+VitalieBtz@users.noreply.github.com>
2023-06-01 08:29:18 -0400

    Update swapNumbers.java

--- src/week02/swapNumbers.java
+++ src/week02/swapNumbers.java
@@ -2,13 +2,17 @@ package week02;
 
 public class swapNumbers {
 
-    public static void swapNumbers(int a, int b){
+    public static void swapNumbers(int x, int y){
 
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        // int x = 5;
+        // int y = 10;
 
-        System.out.println("a = " + a + "\nb = " + b);
+        x = x + y;      // 5 + 10 = 15.                 x = 15
+        y = x - y;      // 15 - 10 = 5.                 y = 5
+        x = x - y;      // 15 - 5 = 10.                 x = 10
+
+        System.out.println("x =  " + x);
+        System.out.println("y =  " + y);
 
     }
 }
