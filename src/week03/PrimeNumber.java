10474202473376b44e7e6b11bbab818cbc6b7377
VitalieBtz <botezatu.v@gmail.com>
2023-06-01 16:45:17 -0400

    created week03

---
+++ src/week03/PrimeNumber.java
@@ -0,0 +1,8 @@
+package week03;
+
+public class PrimeNumber {
+}
+/*
+Numbers -- Prime Number
+Write a method that can check if a number is prime or not.
+ */
---
+++ src/week03/ReverseNegativeNumber.java
@@ -0,0 +1,9 @@
+package week03;
+
+public class ReverseNegativeNumber {
+}
+/*
+ Numbers -- Reverse negative number
+Write a return method that can reverse digits of a negative number and return it as int
+input: -35  output: -53
+ */
