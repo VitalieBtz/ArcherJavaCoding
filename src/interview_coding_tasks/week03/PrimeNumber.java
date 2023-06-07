570672d580f3079a62cd088590d06ae566b5b37f
RaushanSman <Raushansman@gmail.com>
2023-06-07 06:51:53 -0700

    tasks of week 3 created

---
+++ src/interview_coding_tasks/week03/PrimeNumber.java
@@ -0,0 +1,27 @@
+package interview_coding_tasks.week03;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+
+        int num = 20;
+
+        for (int i = 1; i <= num; i++) {
+            int count = 0;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+
+            if (count == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
+
+
+
