f78e02ff6136d77f5c728fd6ce007f804f998b17
VitalieBtz <botezatu.v@gmail.com>
2023-06-08 11:55:38 -0400

    week04 tasks added

---
+++ src/week04/FrequencyOfCharacters.java
@@ -0,0 +1,9 @@
+package week04;
+
+public class FrequencyOfCharacters {
+}
+/*
+String -- Frequency of Characters
+Write a return method that can find the frequency of characters
+Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
