e20ea08c2f84cd13f8b83c6305666311be3c9f54
sara54c <sara5453c@gmail.com>
2023-06-14 08:35:38 -0400

    update RemoveDuplicates class

--- src/week4/RemoveDuplicates.java
+++ src/week4/RemoveDuplicates.java
@@ -1,4 +1,37 @@
 package week4;
 
 public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("xxxtttzzzzh"));
+    }
+
+
+
+    public static String removeDuplicates(String str){
+
+        String result = ""; //because string is immutable we cannot remove the string, we need new string object
+
+        for (int i = 0; i < str.length(); i++) { //  add each character from str to result one by one with iterating.
+            if (result.contains(""+ str.charAt(i))) { // if the character is already contained in new one,
+                continue; // skip
+            }
+
+            result = result + str.charAt(i);// otherwise we will continue to add
+        }
+
+      return result;
+
+
+    }
+
 }
+
+/*
+➡️ String -- Remove Duplicates
+Write a return method that can remove the duplicated values from String
+Ex: removeDup("AAABBBCCC") ==> ABC
+
+
+
+ */
