55fedeaae2683e78e1c7007f348371c14ee032eb
VitalieBtz <botezatu.v@gmail.com>
2023-06-14 08:54:57 -0400

    added wk4 solutions

--- src/week04/RemoveDuplicates.java
+++ src/week04/RemoveDuplicates.java
@@ -1,6 +1,26 @@
 package week04;
 
 public class RemoveDuplicates {
+
+    public static String removeDuplicates(String string){
+
+        String result = "";
+
+        for (int i = 0; i < string.length(); i++) {
+
+            if (!result.contains(""+string.charAt(i))){
+                result += string.charAt(i);
+            }
+        }
+
+        /* ----------- Second Approach --------------------
+
+        result = new LinkedHashSet<>(Arrays.asList(string.split(""))).toString();
+        result = result.replace(", ", "").replace("[", "").replace("]", "");
+        */
+
+        return result;
+    }
 }
 /*
 String -- Remove Duplicates
--- src/week04/SameLetters.java
+++ src/week04/SameLetters.java
@@ -1,6 +1,34 @@
 package week04;
 
+import java.util.Arrays;
+
 public class SameLetters {
+
+    public static boolean sameLetters(String a, String b){
+
+        a = a.replaceAll(" ", "");
+        b = b.replaceAll(" ", "");
+
+        if (a.length() != b.length()){
+            return false;
+        }
+
+        char[] arrayA = a.toCharArray();
+        char[] arrayB = b.toCharArray();
+
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+
+        String aSorted = "";
+        String bSorted = "";
+
+        for(int i = 0; i < arrayA.length-1; i++){
+
+            aSorted += arrayA[i];
+            bSorted += arrayB[i];
+        }
+        return aSorted.equals(bSorted);
+    }
 }
 /*
 String -- Same letters
