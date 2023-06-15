1bac18c3e079716d5d358cfae66a163335f7f424
VitalieBtz <botezatu.v@gmail.com>
2023-06-15 16:11:13 -0400

    added week05 tasks

---
+++ src/week05/ReverseString.java
@@ -0,0 +1,9 @@
+package week05;
+
+public class ReverseString {
+}
+/*
+String -- Reverse
+ Write a function that can reverse a String
+ Ex: Reverse("ABCD"); ==> DCBA
+ */
---
+++ src/week05/UniqueCharacters.java
@@ -0,0 +1,9 @@
+package week05;
+
+public class UniqueCharacters {
+}
+/*
+String - Find the unique
+ Write a return method that can find the unique characters from the String
+ Ex: unique("AAABBBCCCDEF") ==> "DEF";
+ */
