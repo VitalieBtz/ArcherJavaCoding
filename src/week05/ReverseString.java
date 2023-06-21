a9e64361d91f0f54380315a66434ff1124fe8ad1
Niiazbekov_07 <niyazbekovshblt@gmail.com>
2023-06-20 20:41:39 -0500

    Tasks week 5

--- src/week05/ReverseString.java
+++ src/week05/ReverseString.java
@@ -1,6 +1,29 @@
 package week05;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class ReverseString {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your example: ");
+        String ex = input.next();
+
+        String result = "";
+
+        for (int i = ex.length()-1; i >= 0; i--) {
+
+            result += ex.charAt(i) + "";
+
+        }
+
+        System.out.println(result);
+
+
+    }
+
 }
 /*
 String -- Reverse
--- src/week05/UniqueCharacters.java
+++ src/week05/UniqueCharacters.java
@@ -1,6 +1,34 @@
 package week05;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
 public class UniqueCharacters {
+    public static void main(String[] args) {
+
+        System.out.println(UniqueCharacters.FindUniqueEX1("AAABBBCCCDEF"));
+
+        }
+
+
+    public static String FindUniqueEX1(String str){
+
+        String UniqueCharacter = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(str.indexOf(ch) == str.lastIndexOf(ch)){
+                UniqueCharacter += ch;
+            }
+
+        }
+
+        return UniqueCharacter;
+
+    }
+
+
 }
 /*
 String - Find the unique
