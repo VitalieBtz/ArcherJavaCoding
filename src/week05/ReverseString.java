05c05fa7e0790a79852a94cc0c2fcb8c264cd580
VitalieBtz <botezatu.v@gmail.com>
2023-06-29 09:17:29 -0400

    added week05 tasks solutions

--- src/week05/ReverseString.java
+++ src/week05/ReverseString.java
@@ -1,6 +1,25 @@
 package week05;
 
 public class ReverseString {
+
+    public static String reverseString(String str){
+
+        String reversed = "";
+
+        for (int i = str.length()-1; i >= 0; i--){
+            reversed += str.charAt(i);
+        }
+
+        /*
+        //--------------Approach using StringBuilder------------------//
+
+        StringBuilder sB = new StringBuilder();
+
+        reversed = sB.append(str).reverse().toString();
+         */
+
+        return reversed;
+    }
 }
 /*
 String -- Reverse
--- src/week05/UniqueCharacters.java
+++ src/week05/UniqueCharacters.java
@@ -1,6 +1,21 @@
 package week05;
 
 public class UniqueCharacters {
+
+    public static String uniqueCharacters(String str){
+
+        String uniqueCharacters = "";
+
+        for (int i = 0; i < str.length(); i++){
+
+            char eachCharacter = str.charAt(i);
+
+            if (str.indexOf(eachCharacter) == str.lastIndexOf(eachCharacter)){
+                uniqueCharacters += eachCharacter;
+            }
+        }
+        return uniqueCharacters;
+    }
 }
 /*
 String - Find the unique
