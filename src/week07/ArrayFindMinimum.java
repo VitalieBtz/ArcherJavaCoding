2e74bb2cb91fa3cb7b90965dbf23a1b54bf61bdd
Hido_Cydeo <hidoyatnuridinova@gmail>
2023-07-05 01:54:22 -0400

    W7 Array Find Minimum

--- src/week07/ArrayFindMinimum.java
+++ src/week07/ArrayFindMinimum.java
@@ -1,7 +1,28 @@
 package week07;
 
 public class ArrayFindMinimum {
-}
+
+    public static void main(String[] args) {
+
+        int[] array = {12,4,8,3,9};
+        System.out.println(MinNumber(array));
+    }
+
+    public static int MinNumber(int[] arr1) {
+
+        int min = arr1[0];
+
+        for (int i = 0; i < arr1.length; i++) {
+
+            if (min > arr1[i]) {
+                min = arr1[i];
+            }
+        }
+
+        return min;
+    }
+
+    }
 /*
 Write a method that can find the minimum number from an int Array.
  */
