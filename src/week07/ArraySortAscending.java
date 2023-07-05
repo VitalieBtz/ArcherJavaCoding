a5298dc72217400527e701663f598dd84b86e7fe
vitavoloshchuk <vitavoloshchuk@gmail.com>
2023-07-05 10:07:20 -0400

    vita completed Week7 tasks

--- src/week07/ArraySortAscending.java
+++ src/week07/ArraySortAscending.java
@@ -1,9 +1,29 @@
 package week07;
 
+import java.util.Arrays;
+
 public class ArraySortAscending {
+
+    /*
+    Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
+    Ex: int[] arr = {10, 9, 8, 7};
+    arr = Sort(arr); ==>{ 7, 8, 9, 10};
+     */
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortAscending(10, 9, 8, 7)));
+    }
+
+    public static int[] sortAscending(int... arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
 }
-/*
-Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
-Ex: int[] arr = {10, 9, 8, 7};
-arr = Sort(arr); ==>{ 7, 8, 9, 10};
- */
--- src/week07/ArraySortDescending.java
+++ src/week07/ArraySortDescending.java
@@ -1,9 +1,31 @@
 package week07;
 
+import java.util.Arrays;
+
 public class ArraySortDescending {
-}
+
 /*
 Write a return method that can sort an int array in descending order without using the sort method of the Arrays class
 Ex: int[] arr = {10,20,7, 8, 90};
 arr = Sort(arr); ==> {90, 20, 10, 8, 7};
  */
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortDescending(10,20,7, 8, 90)));
+    }
+
+    public static int[] sortDescending(int... arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+}
