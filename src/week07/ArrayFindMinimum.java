a45ee3cdf5c3092ce968cd148c4d981ae608ab72
anis_k <aanniissk@gmail.com>
2023-07-06 19:45:04 -0400

    tasks added

--- src/week07/ArrayFindMinimum.java
+++ src/week07/ArrayFindMinimum.java
@@ -1,6 +1,23 @@
 package week07;
 
 public class ArrayFindMinimum {
+public static int mini(int [] a){
+    int l=a[0];
+    for (int i : a) {
+
+        if(l>=i){
+            l=i;
+        }
+    }
+ return l;
+}
+
+    public static void main(String[] args) {
+    int [ ]a={123,3,4,55,555,666};
+        System.out.println(mini(a));
+
+
+    }
 }
 /*
 Write a method that can find the minimum number from an int Array.
--- src/week07/ArraySortAscending.java
+++ src/week07/ArraySortAscending.java
@@ -1,6 +1,31 @@
 package week07;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArraySortAscending {
+    public static int[] sort(int []a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int aaa = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = aaa;
+                }
+            }
+
+        }
+
+        return a;
+    }
+    public static void main(String[] args) {
+        int []a={13,2,34,4,55};
+        System.out.println(Arrays.toString(sort(a)));
+
+
+    }
+
 }
 /*
 Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
