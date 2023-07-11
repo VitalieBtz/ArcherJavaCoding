3447a541a36211fe08d0ae1b3a4529283da7b21d
sami <samirabih@live.com>
2023-07-11 01:34:15 -0400

    Created Week8 Task

--- src/week08/MoveZerosToTheEnd.java
+++ src/week08/MoveZerosToTheEnd.java
@@ -1,6 +1,29 @@
 package week08;
 
+import java.util.Arrays;
+
 public class MoveZerosToTheEnd {
+    public static void main(String[] args) {
+        int [] array = {1,0,20,0,30,0,4};
+        System.out.println(Arrays.toString(zeroToEnd(array)));
+    }
+
+    public static int [] zeroToEnd(int [] array){
+
+        for (int i = 0; i < array.length; i++) { //{1,0,20,0,30,0,4}
+            for (int j = i+1; j < array.length; j++) {
+                if (array [i] == 0){
+                    array [i] = array[j];
+                    array[j] = 0;
+                }
+
+            }
+
+        }
+        return array;
+    }
+
+
 }
 /*
 Write a method that can move all the zeros to last indexes of the array (Do Not Use Sort Method)
--- src/week08/nUniqueIntegersThatSumUpTo0.java
+++ src/week08/nUniqueIntegersThatSumUpTo0.java
@@ -1,6 +1,29 @@
 package week08;
 
+import java.util.Arrays;
+
 public class nUniqueIntegersThatSumUpTo0 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(uniqueArray(4)));
+    }
+
+    public static int [] uniqueArray(int N){
+
+        int[] result = new int[N];//4
+        int sum = 0;
+
+        for (int i = 0; i < N - 1; i++) { // {0,1,2,}
+            result[i] = i;
+            sum += i;
+        }
+
+        result[N -1] = -sum;
+
+        return result;
+
+
+    }
 }
 /*
      Array - N unique integers that sum up to 0
